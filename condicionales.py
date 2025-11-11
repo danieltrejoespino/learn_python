41468c5870bc1202d4e377b8852781597f428712
Daniel Trejo <daniel.trejo.espino@gmail.com>
2025-11-10 23:07:51 -0600

    Practicando python

--- condicionales.py
+++
@@ -1,9 +0,0 @@
-edad = int(input('Ingresa tu edad: \n'))
-
-if edad >= 19:
-  print('Eres mayor de edad')
-else:
-  print('Eres menor de edad')
-  
-  
-  
---
+++ listas/methods_list.py
@@ -0,0 +1,9 @@
+import os 
+os.system('cls')
+
+lista = [1,2,3,4]
+
+lista.append(1) #Agregar elementos al final
+
+
+print(lista)
